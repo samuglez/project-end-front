b8528cd5e74c1d39469c8e9383a257b847dc51f3
unknown <samuglez90@gmail.com>
2025-02-28 13:02:18 +0100

    Instalado Tailwind y DaissyUI

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import "./HomePage.css";
+
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <button class="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default HomePage;
